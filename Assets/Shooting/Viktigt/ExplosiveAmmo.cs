c764dd8ad3db9056907c2521d452787582d31573
karllanden <62596475+karllanden@users.noreply.github.com>
2020-03-26 10:38:56 +0100

    Added shooting :+1:

---
+++ Assets/Shooting/Viktigt/ExplosiveAmmo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveAmmo : MonoBehaviour
+{
+    float velocity;
+    Vector3 direction;
+    float explosionRadius = 5, damage = 10;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceThisFrame = velocity * Time.deltaTime;
+        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        
+        for(int i =0; i < allEnemies.Length; i++)
+        {
+            if(allEnemies.Length == 0)
+            {
+                break;
+            }
+            Vector3 distance = allEnemies[i].transform.position - this.transform.position;
+            if(distance.magnitude < explosionRadius)
+            {
+                //take damage metod här för fiender##
+            }
+        }
+        Vector3 distanceToPlayer = player.transform.position - this.transform.position;
+        if(distanceToPlayer.magnitude < explosionRadius)
+        {
+            //take damage metod här för spelar objektet
+        }
+
+        Destroy(this.gameObject);
+    }
+    public void GetValues(Transform tf, float v, Vector3 dir)
+    {
+        this.transform.rotation = tf.rotation;
+        this.transform.position = tf.position;
+        velocity = v;
+        direction = dir;
+    }
+}
---
+++ Assets/Shooting/Viktigt/GunShotScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShotScript : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    public int maxAmmo, currentAmmo;
+    float fireCd = 0.3f, timeSinceLastShot = 0;
+    float shotSpeed = 15;
+    Vector3 direction;
+    GameObject target, hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //if(hand != null)
+       //{
+            target = GameObject.FindGameObjectWithTag("Target");
+
+            direction = target.transform.position - this.transform.position;
+            timeSinceLastShot += Time.deltaTime;
+
+            if (Input.GetMouseButton(0) == true && timeSinceLastShot > fireCd)
+            {
+                Shoot();
+                timeSinceLastShot = 0;
+            }
+      // }
+       
+
+    }
+
+    void Shoot()
+    {
+        GameObject createBullet = GameObject.Instantiate(bullet);
+        Bullet firedBullet = createBullet.GetComponent<Bullet>();
+        firedBullet.GetValues(this.transform, shotSpeed, direction);
+    }
+}
