9b3a4a2b05b35e557b0db7b57e3a364c26980908
Whampaz <62597007+Whampaz@users.noreply.github.com>
2020-04-01 12:43:46 +0200

    Healthbar added

--- Assets/PlayerScripts/PlayerHealthScript.cs
+++ Assets/PlayerScripts/PlayerHealthScript.cs
@@ -1,28 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealthScript : MonoBehaviour
 {
     [SerializeField] float maxHealth, currentHealth;
+    //[Header("Unity Stuff")]
+    //public Image healthBar;
     // Start is called before the first frame update
+
+    public HealthBar healthBar;
+
     void Start()
     {
+        maxHealth = 100;
+        currentHealth = maxHealth;
 
+        healthBar.SetMaxhealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentHealth <= 0)
+        
+        //healthBar.fillAmount = currentHealth / maxHealth;
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Die();
+            takeDamage(10);
         }
     }
 
     public void takeDamage(float damageTaken)
     {
-        maxHealth -= damageTaken;
+        currentHealth -= damageTaken;
+
+        healthBar.SetHealth(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
     }
 
     void Die()
