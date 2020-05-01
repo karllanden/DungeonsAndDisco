9135b8286f5f2b7064e4d43e8b5f01f84c6b4cf4
Whampaz <62597007+Whampaz@users.noreply.github.com>
2020-05-01 10:00:55 +0200

    GitMerge
    fish, car, streetlights, grouncheck

---
+++ Assets/FishRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishRotation : MonoBehaviour
+{
+    [SerializeField]
+    public Transform target;
+
+    [SerializeField]
+    int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(target.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
--- Assets/PlayerScripts/PlayerMovementCC.cs
+++ Assets/PlayerScripts/PlayerMovementCC.cs
@@ -12,11 +12,21 @@ public class PlayerMovementCC : MonoBehaviour
 
     Vector3 x = new Vector3(1,1,1);
 
+    [SerializeField]
+    public GameObject groundCheck;
+
+    float gravity = -9.8f;
+    [SerializeField]
+    float gravityMultiplier;
 
 
     void FixedUpdate()
     {
-
+        if (groundCheck.transform.position.y >= -0.8)
+        {
+            Vector3 moveY = new Vector3(0, gravity * Time.deltaTime * gravityMultiplier, 0);
+            controller.Move(Time.deltaTime * moveY);
+        }
         
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
