d64ed90f4bf471aa4d7434ce00d8b2dc33b175b7
Datrex95 <62597027+Datrex95@users.noreply.github.com>
2020-05-01 22:04:10 -0800

    Music effekt
    La till en trigger event när du kommer till en viss rum, kan du spela upp muik

---
+++ Assets/RoomMusic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMusic : MonoBehaviour
+{
+    public string musicName;
+    BoxCollider boxCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Touched");
+            FindObjectOfType<MusicManager>().Play(musicName);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //if (other.gameObject.CompareTag("Player"))
+        //{
+        //    FindObjectOfType<AudioManager>().Stop("ThemeSong");
+        //}
+    }
+}
---
+++ Assets/Shooting/Sound effects/MusicManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class MusicManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Music[] musicColletion;
+    public AudioSource audioManager;
+    void Awake()
+    {
+        audioManager = GetComponent<AudioSource>();
+        foreach (Music music in musicColletion)
+        {
+            music.source = gameObject.AddComponent<AudioSource>();
+            music.source.name = music.name;
+            music.source.clip = music.clip;
+            music.source.pitch = music.pitch;
+            music.source.volume = music.volume;
+        }
+    }
+
+    // Update is called once per frame
+    public void Play(string name)
+    {
+        Music m = Array.Find(musicColletion, music => music.name == name);
+        audioManager = m.source;
+        audioManager.Play();
+    }
+
+    //public void Stop(string name)
+    //{
+    //    Music m = Array.Find(musicColletion, music => music.name == name);
+    //    audioManager = m.source;
+    //    audioManager.Stop();
+    //}
+}
